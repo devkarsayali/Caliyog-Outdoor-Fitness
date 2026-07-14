802a1912d2eee92081b11e4e00941864998ea790
Your Name <you@example.com>
2026-07-14 16:23:38 +0530

    comment to railway URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,8 @@
+const express
+
 const API_URL =
- "https://caliyog-fitness-backend-production-2144.up.railway.app";
- //"http://10.93.11.10:5000"
+ //"https://caliyog-fitness-backend-production-2144.up.railway.app";
+ "http://10.93.11.13:5000"
 export default API_BASE_URL;
 
 
